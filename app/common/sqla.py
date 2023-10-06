08cc39ac6be0520ad08e2cf86619fd738e546150
niqzart <qwert45hi@yandex.ru>
2023-10-07 02:02:46 +0300

    feat: database middleware for fastapi

--- app/common/sqla.py
+++ app/common/sqla.py
@@ -1,56 +1,13 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
-from contextvars import ContextVar, Token
-from types import TracebackType
+from contextvars import ContextVar
 from typing import Any, Self, TypeVar
 
 from sqlalchemy import Result, Select, select
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker as Sessionmaker
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.requests import Request
-from starlette.responses import Response
-from starlette.types import ASGIApp
+from sqlalchemy.ext.asyncio import AsyncSession
 
-session: ContextVar[AsyncSession | None] = ContextVar("session", default=None)
-
-
-class DBSessionMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: ASGIApp, sessionmaker: Sessionmaker[AsyncSession]) -> None:
-        super().__init__(app)
-        self.sessionmaker: Sessionmaker[AsyncSession] = sessionmaker
-        self.token: Token[AsyncSession | None] | None = None
-
-    async def __aenter__(self) -> Self:
-        self.token = session.set(self.sessionmaker())
-        return self
-
-    async def __aexit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_val: BaseException | None,  # noqa: U100
-        exc_tb: TracebackType | None,  # noqa: U100
-    ) -> None:
-        current_session = session.get()
-        if current_session is None:
-            raise RuntimeError("Session is None before closing")
-
-        if exc_type is not None:
-            await current_session.rollback()
-        else:
-            await current_session.commit()
-
-        await current_session.close()
-        # might need a `session.reset(self.token)`
-
-    async def dispatch(
-        self,
-        request: Request,
-        call_next: RequestResponseEndpoint,
-    ) -> Response:
-        async with self:
-            return await call_next(request)
-
+session_context: ContextVar[AsyncSession | None] = ContextVar("session", default=None)
 
 t = TypeVar("t", bound=Any)
 
@@ -59,10 +16,10 @@ class DBController:
     @property
     def session(self) -> AsyncSession:
         """Return an instance of Session local to the current context"""
-        result = session.get()
-        if result is None:
+        session = session_context.get()
+        if session is None:
             raise ValueError("Session not initialized")
-        return result
+        return session
 
     async def get_first(self, stmt: Select[tuple[t]]) -> t | None:
         result: Result[tuple[t]] = await self.session.execute(stmt)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from collections.abc import AsyncIterator
+from collections.abc import AsyncIterator, Awaitable, Callable
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
 
-from app.common.config import DATABASE_RESET, PRODUCTION_MODE, Base, db_url, engine
+from app.common.config import (
+    DATABASE_RESET,
+    PRODUCTION_MODE,
+    Base,
+    db_url,
+    engine,
+    sessionmaker,
+)
+from app.common.sqla import session_context
 
 
 @asynccontextmanager
@@ -19,3 +29,13 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware("http")
+async def database_session_middleware(
+    request: Request,
+    call_next: Callable[[Request], Awaitable[Response]],
+) -> Response:
+    async with sessionmaker.begin() as session:
+        session_context.set(session)
+        return await call_next(request)
