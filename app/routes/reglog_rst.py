1cac381d3820f96a762ef61dd6cdbe408163331f
niqzart <qwert45hi@yandex.ru>
2023-10-07 18:48:10 +0300

    feat: documenting error responses

--- app/routes/reglog_rst.py
+++ app/routes/reglog_rst.py
@@ -1,32 +1,47 @@
-from fastapi import APIRouter, HTTPException
-from starlette.status import HTTP_409_CONFLICT
+from fastapi import APIRouter
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 
-from app.common.exceptions import UnauthorizedException
+from app.common.responses import Responses
 from app.models.users_db import User, UserPasswordModel
 
 router = APIRouter(tags=["reglog"])
 
 
-@router.post("/signup", response_model=User.FullModel)
+class SignupResponses(Responses):
+    EMAIL_IN_USE = (HTTP_409_CONFLICT, "Email already in use")
+
+
+@router.post(
+    "/signup",
+    response_model=User.FullModel,
+    responses=SignupResponses.responses(),
+)
 async def signup(user_data: UserPasswordModel) -> User:
     if await User.find_first_by_kwargs(email=user_data.email) is not None:
-        raise HTTPException(
-            status_code=HTTP_409_CONFLICT, detail="Email already in use"
-        )
+        raise SignupResponses.EMAIL_IN_USE.value
 
     # TODO send email
     # TODO create token & add as headers
     return await User.create(**user_data.model_dump())
 
 
-@router.post("/signin", response_model=User.FullModel)
+class SigninResponses(Responses):
+    USER_NOT_FOUND = (HTTP_401_UNAUTHORIZED, User.not_found_text)
+    WRONG_PASSWORD = (HTTP_401_UNAUTHORIZED, "Wrong password")
+
+
+@router.post(
+    "/signin",
+    response_model=User.FullModel,
+    responses=SigninResponses.responses(),
+)
 async def signin(user_data: User.InputModel) -> User:
     user = await User.find_first_by_kwargs(email=user_data.email)
     if user is None:
-        raise UnauthorizedException(detail=User.not_found_text)
+        raise SigninResponses.USER_NOT_FOUND.value
 
     if not user.is_password_valid(user_data.password):
-        raise UnauthorizedException(detail="Wrong password")
+        raise SigninResponses.WRONG_PASSWORD.value
 
     # TODO create token & add as headers
     return user
--- app/routes/users_mub.py
+++ app/routes/users_mub.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from starlette.status import HTTP_404_NOT_FOUND
 
+from app.common.responses import Responses
 from app.models.users_db import User, UserPasswordModel
 
 router = APIRouter(tags=["users mub"])
@@ -11,19 +12,31 @@ async def create_user(user_data: UserPasswordModel) -> User:
     return await User.create(**user_data.model_dump())
 
 
-@router.get("/{user_id}", response_model=User.FullModel)
+class UserResponses(Responses):
+    USER_NOT_FOUND = (HTTP_404_NOT_FOUND, User.not_found_text)
+
+
+@router.get(
+    "/{user_id}",
+    response_model=User.FullModel,
+    responses=UserResponses.responses(),
+)
 async def retrieve_user(user_id: int) -> User:
     user = await User.find_first_by_id(user_id)
     if user is None:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=User.not_found_text)
+        raise UserResponses.USER_NOT_FOUND.value
     return user
 
 
-@router.put("/{user_id}", response_model=User.FullModel)
+@router.put(
+    "/{user_id}",
+    response_model=User.FullModel,
+    responses=UserResponses.responses(),
+)
 async def update_user(user_id: int, user_data: User.PatchModel) -> User:
     user = await User.find_first_by_id(user_id)
     if user is None:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=User.not_found_text)
+        raise UserResponses.USER_NOT_FOUND.value
 
     user.update(**user_data.model_dump(exclude_defaults=True))
     return user
