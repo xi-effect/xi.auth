aa645bba5c9f71fe8c8c8078e0920c8756ffa33a
niqzart <qwert45hi@yandex.ru>
2024-05-21 20:33:33 +0300

    fix: replace asyncpg with psycopg and remove NullPool

--- app/common/config.py
+++ app/common/config.py
@@ -1,6 +1,7 @@
+import asyncio
+import sys
 from os import getenv
 from pathlib import Path
-from sys import modules
 
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
@@ -14,17 +15,20 @@ from app.common.sqla import MappingBase
 
 current_directory: Path = Path.cwd()
 
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 load_dotenv(current_directory / ".env")
 
 AVATARS_PATH: Path = current_directory / "avatars"
 
 PRODUCTION_MODE: bool = getenv("PRODUCTION", "0") == "1"
-TESTING_MODE: bool = "pytest" in modules
+TESTING_MODE: bool = "pytest" in sys.modules
 
 COOKIE_DOMAIN: str = getenv("COOKIE_DOMAIN", "localhost")
 DATABASE_MIGRATED: bool = getenv("DATABASE_MIGRATED", "0") == "1"
 
-DB_URL: str = getenv("DB_LINK", "postgresql+asyncpg://test:test@localhost:5432/test")
+DB_URL: str = getenv("DB_LINK", "postgresql+psycopg://test:test@localhost:5432/test")
 DB_SCHEMA: str | None = getenv("DB_SCHEMA", None)
 
 MQ_URL: str = getenv("MQ_URL", "amqp://guest:guest@localhost/")
